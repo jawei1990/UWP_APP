f5fecbce8f4460f2df9c69d058350cf689e8c550
Sofia-Chein <sofia.chien@wiseomeinc.com>
2020-11-30 15:48:25 +0800

    [U] Add WFA serial port open demo application.

---
+++ sample/LaserUsbDemo/LaserUsbDemo/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace LaserUsbDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.BtnDeviceStatus = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.Uart_Rx_Handle);
+            // 
+            // BtnDeviceStatus
+            // 
+            this.BtnDeviceStatus.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDeviceStatus.Location = new System.Drawing.Point(324, 36);
+            this.BtnDeviceStatus.Name = "BtnDeviceStatus";
+            this.BtnDeviceStatus.Size = new System.Drawing.Size(210, 73);
+            this.BtnDeviceStatus.TabIndex = 0;
+            this.BtnDeviceStatus.Text = "Connect";
+            this.BtnDeviceStatus.UseVisualStyleBackColor = true;
+            this.BtnDeviceStatus.Click += new System.EventHandler(this.BtnDeviceStatus_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.PageTabList;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Device Status:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Location = new System.Drawing.Point(42, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(245, 31);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("BtnRefresh.Image")));
+            this.BtnRefresh.Location = new System.Drawing.Point(563, 34);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(77, 75);
+            this.BtnRefresh.TabIndex = 3;
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnDeviceStatus);
+            this.Name = "Form1";
+            this.Text = "Laser Usb Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button BtnDeviceStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button BtnRefresh;
+    }
+}
+
---
+++ sample/LaserUsbDemo/LaserUsbDemo/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+namespace LaserUsbDemo
+{
+    public partial class Form1 : Form
+    {
+        Object[] comboBoxData = new object[] {
+                            "COM1","COM2","COM3","COM4","COM5","COM6","COM7","COM8","COM9","COM10",
+                            "COM11","COM12","COM13","COM14","COM15","COM16","COM17","COM18","COM19","COM20",
+                            "COM21","COM22","COM23","COM24","COM25","COM26","COM27","COM28","COM29","COM30",
+                            "COM31","COM32","COM33","COM34","COM35","COM36","COM37","COM38","COM39","COM40",
+                            "COM41","COM42","COM43","COM44","COM45","COM46","COM47","COM48","COM49","COM50"};
+        bool isDeviceConnected = false;
+        string device_port = "";
+        string device_port_num = "";
+        List <ComPort> SerialPort = new List<ComPort>();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(comboBoxData);
+            getComPort();
+        }
+
+        private void getComPort()
+        {
+            SerialPort = GetSerialPort2();
+            string pattern_name = "CH340"; 
+            string pattern_port = "COM";
+
+            Regex rgx_name = new Regex(pattern_name);
+            Regex rgx_port = new Regex(pattern_port);
+
+            for (int i = 0; i < SerialPort.Count; i++)
+            {
+                Match match_name = rgx_name.Match(SerialPort[i].description);
+                Match match_port = rgx_port.Match(SerialPort[i].description);
+
+                if (match_name.Success)
+                {
+                    char[] data = SerialPort[i].description.ToCharArray();
+
+                    for (int j = match_port.Index+3; j < data.Length; j++)
+                    {
+                        if (!string.Equals(data[j].ToString(), ")"))
+                            device_port_num += data[j].ToString();
+                    }
+
+                    device_port = "COM" + device_port_num;
+                    Console.WriteLine("port:" + device_port);
+
+                    // 從 0 開始算
+                    comboBox1.SelectedIndex = Int16.Parse(device_port_num) -1 ; 
+                    Console.WriteLine("---- found -----:" + SerialPort[i].description);
+                }
+            }
+        }
+
+        struct ComPort
+        {
+            public string name;
+            public string description;
+
+            public ComPort(string name, string description)
+            {
+                this.name = name;
+                this.description = description;
+            }
+        }
+
+        // Method 2 function
+        private List<ComPort> GetSerialPort2()
+        {
+            Console.WriteLine("------- GetSerialPort2 -----");
+            List<ComPort> ports = new List<ComPort>();
+            ManagementClass processClass = new ManagementClass("Win32_PnPEntity");
+
+            ManagementObjectCollection Ports = processClass.GetInstances();
+           // string device = "No recognized";
+            foreach (ManagementObject port in Ports)
+            {
+                if (port.GetPropertyValue("Name") != null)
+                    if (port.GetPropertyValue("Name").ToString().Contains("USB") &&
+                        port.GetPropertyValue("Name").ToString().Contains("COM"))
+                    {
+                        ComPort c = new ComPort();
+                        c.name = port.GetPropertyValue("DeviceID").ToString();
+                        //               Console.WriteLine("name:" + port.GetPropertyValue("DeviceID").ToString());
+                        c.description = port.GetPropertyValue("Caption").ToString();
+                        //                Console.WriteLine("description:" + port.GetPropertyValue("Caption").ToString());
+                        ports.Add(c);
+                    }
+            }
+            return ports;
+        }
+
+        private void CloseSerialPort()
+        {
+            try
+            {
+                serialPort1.Close();
+                BtnDeviceStatus.Text = "Connect";
+                Console.WriteLine("------- CloseSerialPort -----");
+                isDeviceConnected = false;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //System.Environment.Exit(1);
+            }
+        }
+
+        private void OpenSerialPort(string portName)
+        {
+            try
+            {
+                serialPort1.PortName = portName;
+                serialPort1.Open();
+                BtnDeviceStatus.Text = "Disconnect";
+                Console.WriteLine("------- OpenSerialPort -----");
+                isDeviceConnected = true;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //System.Environment.Exit(1);
+            }
+        }
+
+        private void BtnDeviceStatus_Click(object sender, EventArgs e)
+        {
+            if (isDeviceConnected)
+            {
+                CloseSerialPort();
+            }
+            else
+            {
+                OpenSerialPort(device_port);
+            }  
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            if(!isDeviceConnected)
+                getComPort();
+        }
+
+        private void Uart_Rx_Handle(object sender, SerialDataReceivedEventArgs e)
+        {
+
+        }
+    }
+}
